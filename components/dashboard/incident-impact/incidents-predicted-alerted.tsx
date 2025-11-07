d11f804524e28ce172bb983069f8519aa995ba32
Ajith Mathew <ajith1989@gmail.com>
2025-11-07 16:01:11 +0400

    added incidents predicted and alerted

---
+++ components/dashboard/incident-impact/incidents-predicted-alerted.tsx
@@ -0,0 +1,45 @@
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Info } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export default function IncidentsPredictedAlerted() {
+  return (
+    <Card className="@container/card">
+      <CardHeader>
+        <CardDescription className="font-semibold">
+          Incidents - Predicted & Alerted
+        </CardDescription>
+        <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+          93
+        </CardTitle>
+        <CardAction>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Info className="opacity-50 cursor-pointer" size="16" />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>
+                Incidents identified before customer or system impact, based on
+                early signals and predictive telemetry.
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </CardAction>
+      </CardHeader>
+      <CardFooter className="text-sm">
+        <div className="text-muted-foreground">Count</div>
+      </CardFooter>
+    </Card>
+  );
+}
--- components/dashboard/incident-impact/index.tsx
+++ components/dashboard/incident-impact/index.tsx
@@ -1,4 +1,5 @@
 import CostSaved from "./cost-saved";
+import IncidentsPredictedAlerted from "./incidents-predicted-alerted";
 import MTTR from "./mttr";
 import NoiseReduced from "./noise-reduced";
 import TimeSaved from "./time-saved";
@@ -8,8 +9,11 @@ export default function IncidentImpact() {
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
       <MTTR />
       <div className="flex flex-col space-y-4">
-        <CostSaved />
-        <TimeSaved />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <CostSaved />
+          <TimeSaved />
+        </div>
+        <IncidentsPredictedAlerted />
         <NoiseReduced />
       </div>
     </div>
